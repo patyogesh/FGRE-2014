96e443339dda7343e30e52e5e3c7fce630a28e55
patyogesh <patyogesh@gmail.com>
2014-07-10 15:22:11 +0200

    Bug fix

--- net_latency_emu.sh
+++ net_latency_emu.sh
@@ -7,13 +7,13 @@ IP2=10.10.131.81
 
 echo "Initial Delay----"
 fping -e -q  $IP1 $IP2 -c $PingCount 2> out.txt
-cat out.txt | awk '{print $1" "$5" "$8}' | sed 's/\// /g' | sed 's/\%,/ /g'
+cat out.txt | awk '{print $1" "$5" "$8}' | sed 's/\// /g' | sed 's/\%,/ /g' > 1.txt
 
 echo "After Inducing Delay of $Delay----"
 sudo tc qdisc add dev wlan0 root netem delay $Delay
 
 fping -e -q  $IP1 $IP2 -c $PingCount 2> out.txt
-cat out.txt | awk '{print $1" "$5" "$8}' | sed 's/\// /g' | sed 's/\%,/ /g'
+cat out.txt | awk '{print $1" "$5" "$8}' | sed 's/\// /g' | sed 's/\%,/ /g' > 2.txt
 
 sudo tc qdisc del dev wlan0 root netem
 
@@ -21,6 +21,6 @@ echo "More and More bad network condition----"
 sudo tc qdisc add dev wlan0 root netem delay 200ms 40ms 25% loss 15.3% 25% duplicate 1% corrupt 0.1% reorder 5% 50%
 
 fping -e -q  $IP1 $IP2 -c $PingCount 2> out.txt
-cat out.txt | awk '{print $1" "$5" "$8}' | sed 's/\// /g' | sed 's/\%,/ /g'
+cat out.txt | awk '{print $1" "$5" "$8}' | sed 's/\// /g' | sed 's/\%,/ /g' > 3.txt
 
 sudo tc qdisc del dev wlan0 root netem
