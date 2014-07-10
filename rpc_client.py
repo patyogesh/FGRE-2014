f10ceeb99758fe143fbe33c8fcfefaf3c2799e2c
Arun Adiththan <arun@ubuntu.(none)>
2014-07-10 03:36:09 -0700

    Added Getter and Setter methods

--- rpc_client.py
+++ rpc_client.py
@@ -2,7 +2,11 @@ import xmlrpclib
 
 proxy = xmlrpclib.Server('http://localhost:8081', allow_none=True)
 try:
-    print proxy.disp_msg('Hello')
+    #print proxy.disp_msg('Hello')
+    print proxy.set_left_delay(150)
+    print proxy.set_right_delay(220)
+    print proxy.get_delays()
+
 except Exception, err:
     print 'Fault code:', err.faultCode
     print 'Message   :', err.faultString
--- rpc_server.py
+++ rpc_server.py
@@ -3,11 +3,42 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 # server URL
 server = SimpleXMLRPCServer(('localhost', 8081), allow_none=True)
 
-# display received string
-def disp_msg(message):
-	print message
+class DelayHandler:
+	# set delay in left branch
+	def set_left_delay(self, val):
+		self.delay_left = val
+		print self.delay_left
+		return True
 
-server.register_function(disp_msg)
+	# set delay in right branch
+	def set_right_delay(self, val):
+		self.delay_right= val
+		return True
+
+	# set client choice
+	def set_exit_choice(self, val):
+		self.exit_choice = val
+		return True
+
+	# get delays
+	def get_delays(self):
+		delay_tup = (self.delay_left, self.delay_right)
+		return delay_tup
+
+
+# display received string
+#def disp_msg(message):
+#	print message
+
+#server.register_function(disp_msg)
+
+#server.register_function(set_left_delay)
+#server.register_function(set_right_delay)
+#server.register_function(set_exit_choice)
+#server.register_function(get_delays)
+
+server.register_instance(DelayHandler())
+
 try:
     print 'Use Control-C to exit'
     server.serve_forever()
