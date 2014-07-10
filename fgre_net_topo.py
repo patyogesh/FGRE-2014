84ec764cf23134fb816d0fec3b0a93633b3c47aa
Laurent Vanbever <laurent.vanbever@gmail.com>
2014-07-10 12:21:08 +0200

    Simplify

--- fgre_net_topo.py
+++ fgre_net_topo.py
@@ -45,9 +45,9 @@ def fgreTopo():
 
 	info( '*** Configuring ISP \n' )
 	isp = net.get('isp')
-	isp.cmd('ifconfig lo:1 20.0.0.1/16 up')
 	isp.cmd('ifconfig isp-eth0 30.0.1.2/24 up')
 	isp.cmd('ifconfig isp-eth1 30.0.2.2/24 up')
+	isp.cmd('ifconfig lo:1 20.0.0.1/16 up')
 	isp.cmd('arp -s 30.0.1.1 00:0a:aa:bb:cc:da')
 	isp.cmd('arp -s 30.0.2.1 00:0a:aa:bb:cc:db')
 	isp.cmd('ip route add 30.0.0.0/16 via 30.0.2.1 dev isp-eth1')
