87f168bbd6b9729515142b2afa773d5777dc8dee
Laurent Vanbever <laurent.vanbever@gmail.com>
2014-07-11 11:44:11 +0200

    DEBUG

--- h1_ping.sh
+++ h1_ping.sh
@@ -3,5 +3,6 @@ rtt=1
 PingCount=5
 IP1=30.0.1.2
 
+rm host1.txt
 fping -e -q  $IP1 -c $PingCount 2> out.txt
 cat out.txt | awk '{print $1" "$5" "$8}' | sed 's/\// /g' | sed 's/\%,/ /g' > host1.txt
--- h2_ping.sh
+++ h2_ping.sh
@@ -3,5 +3,6 @@ rtt=1
 PingCount=5
 IP1=30.0.2.2
 
+rm host2.txt
 fping -e -q  $IP1 -c $PingCount 2> out.txt
 cat out.txt | awk '{print $1" "$5" "$8}' | sed 's/\// /g' | sed 's/\%,/ /g' > host2.txt
