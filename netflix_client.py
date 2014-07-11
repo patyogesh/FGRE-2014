1de288d828d5cf613661291e8506845a0cdcc0d6
Arun Adiththan <arun@ubuntu.(none)>
2014-07-11 01:44:47 -0700

    netflix client - fixed bug

--- netflix_client.py
+++ netflix_client.py
@@ -5,8 +5,15 @@ import xmlrpclib
 
 proxy = xmlrpclib.Server('http://localhost:8081', allow_none=True)
 try:
-	proxy.set_exit_choice(min(proxy.get_delays()))
+	proxy.set_exit_choice(get_exit_choice(proxy.get_delays()))
 
 except Exception, err:
     print 'Fault code:', err.faultCode
     print 'Message   :', err.faultString
+
+
+def get_exit_choice(tup):
+	if(tup[0]<tup[1]):
+		return 'left'
+	else:
+		return 'right'
