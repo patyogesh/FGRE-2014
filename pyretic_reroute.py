6d131ecda3c46eef9f98788fa4d3b3d70e9440e0
Laurent Vanbever <laurent.vanbever@gmail.com>
2014-07-11 11:28:44 +0200

    DEBUG

--- pyretic_reroute.py
+++ pyretic_reroute.py
@@ -8,8 +8,7 @@ from pyretic.lib.corelib import *
 from pyretic.lib.std import *
 from pyretic.modules.mac_learner import mac_learner
 import threading
-import SocketServer
-from SimpleXMLRPCServer import SimpleThreadedXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 #########################
 ##
