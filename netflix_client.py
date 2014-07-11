b87158273745c364f2324b5edf0ac6646e210d99
Arun Adiththan <arun@ubuntu.(none)>
2014-07-11 01:36:33 -0700

    netflix client added

--- netflix_client.py
+++ netflix_client.py
@@ -5,7 +5,7 @@ import xmlrpclib
 
 proxy = xmlrpclib.Server('http://localhost:8081', allow_none=True)
 try:
-	print proxy.get_delays()
+	proxy.set_exit_choice(min(proxy.get_delays()))
 
 except Exception, err:
     print 'Fault code:', err.faultCode
